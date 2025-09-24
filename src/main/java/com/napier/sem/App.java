9ed9bdb7b19182c619fc04fc5e4ffca91a8edbea
Harris-Taylor <40708546@live.napier.ac.uk>
2025-09-24 12:06:05 +0100

    Added work pl

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,6 +5,6 @@ public class App
     public static void main(String[] args)
     {
         System.out.println("Boo yah!");
-        System.out.println("work pls");
+        System.out.println("work pl");
     }
 }
